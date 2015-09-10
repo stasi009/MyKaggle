5de987aa1ebd1690e8c0560802f310f24141113b
Chuanlin Zhao <cheka008@gmail.com>
2015-09-09 23:47:22 -0700

    try another rf

--- Titanic/learn5_boosttree.py
+++ Titanic/learn5_boosttree.py
@@ -3,33 +3,29 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
-import sklearn.ensemble as skensemble
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.grid_search import RandomizedSearchCV
 from scipy.stats import randint as sp_randint
 
+# ---------------------- load the data
+titanic_train = pd.read_csv("train_processed.csv",index_col="PassengerId")
 
-titanic = pd.DataFrame.from_csv("train_processed.csv")
+feature_names = ["Pclass","Age","SibSp","Parch","Fare","IsMale","Ticket-4digit","Ticket-5digit","Ticket-6digit","Ticket-7digit","Ticket-A","Ticket-C","Ticket-F","Ticket-Others","Ticket-P","Ticket-S","Ticket-W"]
+Xtrain = titanic_train[feature_names]
+ytrain = titanic_train["Survived"]
 
-feature_names = ["Pclass","Age","SibSp","Parch","Fare","IsMale"]
-# feature_names = ["Pclass","Age","SibSp","Parch","Fare","IsMale","EmbarkC","EmbarkQ","EmbarkS"]
-Xtrain = titanic[feature_names]
-ytrain = titanic["Survived"]
-
-
-
+# ---------------------- train
 loss = ['deviance', 'exponential']
-learning_rate = np.logspace(-4,3)
+learning_rate = np.logspace(-5,2)
 n_estimate_dist = sp_randint(100,1000)
-max_depth_dist = sp_randint(1,5)
+max_depth_dist = sp_randint(1,9)
 param_dist = dict(loss=loss,
                 learning_rate=learning_rate,
                 n_estimators=n_estimate_dist,
                 max_depth=max_depth_dist)
 
-
-gbdt = skensemble.GradientBoostingClassifier()
-n_iter_search = 200
-searchcv = RandomizedSearchCV(estimator=gbdt, param_distributions=param_dist,n_iter=n_iter_search)
+gbdt = GradientBoostingClassifier(verbose=1)
+searchcv = RandomizedSearchCV(estimator=gbdt, param_distributions=param_dist,n_iter=200,verbose=1)
 searchcv.fit(Xtrain,ytrain)      
 
 searchcv.best_score_                                  
@@ -37,16 +33,16 @@ searchcv.best_estimator_
 searchcv.best_params_
 
 
-# ---------------------------------------------------- #
-titanic_test = pd.DataFrame.from_csv("test_processed.csv")
+# ---------------------- predict
+titanic_test = pd.read_csv("test_processed.csv",index_col="PassengerId")
 Xtest = titanic_test[feature_names]
 
 predictions = searchcv.predict(Xtest)
 submission = pd.DataFrame({
-        "PassengerId": titanic_test["PassengerId"],
+        "PassengerId": titanic_test.index,
         "Survived": predictions
     })
-submission.to_csv("submit_gbdt2.csv", index=False)
+submission.to_csv("submit_gbdt.csv", index=False)
 
 
 
