9fc65aa64613bdbaacc1b57b53c863a5c7d42b6c
Chuanlin Zhao <cheka008@gmail.com>
2016-02-17 14:23:16 -0800

    code to load train and test dataset, make them into Dmatrix, and save Dmatrix to file, to save future preprocessing time

--- OttoProduct/otto.py
+++ OttoProduct/otto.py
@@ -1,3 +1,19 @@
 ﻿
+import numpy as np
 import pandas as pd
+import xgboost as xgb
+import commfuncs
+
+def save_train_test_dmatrix():
+    """
+    load and save to file, in order to save preprocessing time
+    """
+    trainData = commfuncs.RawTrainData("raw_datas/train.csv")
+    xg_train = xgb.DMatrix( trainData.Xtrain, label=trainData.ytrain)
+    xg_train.save_binary("train.dmatrix")
+
+    testdata = pd.read_csv("raw_datas/test.csv",index_col = "id")
+    xg_test = xgb.DMatrix(testdata)
+    xg_test.save_binary("test.dmatrix")
+
 
