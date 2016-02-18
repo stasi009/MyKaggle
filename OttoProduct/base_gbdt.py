37bffea80282736fba2bc3348c9610f4111aaf0d
Chuanlin Zhao <cheka008@gmail.com>
2016-02-18 11:07:49 -0800

    codes update

--- OttoProduct/base_gbdt.py
+++ OttoProduct/base_gbdt.py
@@ -11,6 +11,10 @@ import commfuncs
 # load train datas
 trainData = commfuncs.RawTrainData("raw_datas/train.csv")
 labels = trainData.unique_labels()
+testdata = pd.read_csv("raw_datas/test.csv",index_col = "id")
+
+xg_entire_train = xgb.DMatrix("train.dmatrix")
+xg_test = xgb.DMatrix("test.dmatrix")
 
 class Predictor(object):
 
@@ -22,13 +26,15 @@ class Predictor(object):
         param['num_class'] = trainData.unique_labels().size
         self.param = param
 
+        self.folds_logloss = []
+        self.folds_best_iterations = []
+
     def cv_train(self):
         num_rounds = self.param["num_rounds"]
         early_stopping_rounds = self.param["early_stop_rounds"]
         num_cv = self.param["num_cv"]
     
         # ------------------- begin cross-validation
-        logloss = 0.0
         folds_predicts = []
 
         kfolds = StratifiedKFold(y=trainData.ytrain,n_folds=num_cv)
@@ -45,9 +51,10 @@ class Predictor(object):
             watchlist = [(xg_train,'train'), (xg_validate, 'validate')]# early stop will check on the last dataset
             bst = xgb.train(self.param, xg_train, num_rounds, watchlist,early_stopping_rounds=early_stopping_rounds)
 
-            logloss += bst.best_score
-            print "best_iteration: %d" % (bst.best_iteration)
+            self.folds_logloss.append(bst.best_score)
             print "best score: %3.2f" % (bst.best_score)
+            self.folds_best_iterations.append(bst.best_iteration)
+            print "best_iteration: %d" % (bst.best_iteration)
 
             # predict on validation set
             predict_on_validate = bst.predict(xg_validate,ntree_limit=bst.best_iteration)
@@ -59,27 +66,49 @@ class Predictor(object):
             predict_on_validate = pd.DataFrame(predict_on_validate,index = fold_xvalidate.index, columns = labels)
             folds_predicts.append(predict_on_validate)
 
-        self.logloss = logloss / num_cv
+        # ------------------- summary
         self.cv_predict_probs = pd.concat(folds_predicts)
-        print "Parameters: %s" % (self.param)
-        print "average logloss: %3.2f" % (self.logloss)
 
-    def refit_save(self,modelname):
+        print "\n******************** TRAIN COMPLETED ********************"
+        print "\nParameters: %s" % (self.param)
+        print "\nfolds best iterations: %s, \nmean=%3.2f, median=%d, STD=%3.2f" % (self.folds_best_iterations,np.mean(self.folds_best_iterations),np.median(self.folds_best_iterations),np.std(self.folds_best_iterations))
+        print "\nfolds logloss: %s \nmean=%3.2f, STD=%3.2f" % (self.folds_logloss,np.mean(self.folds_logloss),np.std(self.folds_logloss))
+
+    def refit_save_model(self,num_rounds, modelname):
+        # -------------------- refit on entire train dataset
+        watchlist = [(xg_entire_train,'entire_train')]
+        self.bst = xgb.train(self.param,xg_entire_train,num_rounds,watchlist)
+
+        # -------------------- save model
         self.bst.save_model(modelname)
 
     def save_meta_features(self,fname):
         self.cv_predict_probs.to_csv("meta_features/%s" % fname,index_labels="id")
 
+    def predict_on_test(self,filename):
+        prediction = self.bst.predict(xg_test)
+        prediction = pd.DataFrame(prediction,index = testdata.index, columns = labels)
 
+        # save prediction into file
+        prediction.to_csv(filename,index_label="id")
+
+
+file_offset = 1
 
 param = {}
-param['max_depth'] = 6
+param['max_depth'] = 10
 param["num_rounds"] = 500
 param["early_stop_rounds"] = 20
 param["num_cv"] = 5
+param["seed"] = 9
 
 predictor = Predictor(param)
 predictor.cv_train()
+
+file_offset += 1
+predictor.save_meta_features("xgb_cv%d.csv"%file_offset)
+predictor.refit_save_model(153,"xgboost%d.xgb"%file_offset)
+predictor.predict_on_test("xgb_predict%d.csv"%file_offset)
 
 
 
